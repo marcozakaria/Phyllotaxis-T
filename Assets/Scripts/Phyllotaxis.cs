01eebde1c24b588efc5f3650c3b6068ded2910e1
Marco Zakaria <marcozakaria@yahoo.com>
2018-11-12 00:24:29 +0200

    Added trail render, lerping option

--- Assets/Scripts/Phyllotaxis.cs
+++ Assets/Scripts/Phyllotaxis.cs
@@ -4,27 +4,81 @@ using UnityEngine;
 
 public class Phyllotaxis : MonoBehaviour
 {
-    public GameObject prefap;
-    public float degree, constant;
-    public int n; // count
-    public float scale;
+    
+    public float degree, scale;
+    public int numberStart;
+    public int stepSize;
+    public int maxIteration;
 
+    //Lerbing
+    public bool useLerping;
+    public float intervalLerp;
+    private bool islerbing;
+    private Vector3 startPosition, endPosition;
+    private float timeStartedLerbing;
+
+    private int currentIteration;
+    private int number;
     private Vector2 PhyllotaxisPosition;
+    private TrailRenderer trailRenderer;
 
-    private void Update()
+    private void Awake()
     {
-        if (Input.GetKey(KeyCode.Space))
+        trailRenderer = GetComponent<TrailRenderer>();
+        number = numberStart;
+        transform.localPosition = CalculatePhyllotXIS(degree, scale, number);
+
+        if (useLerping)
         {
-            PhyllotaxisPosition = CalculatePhyllotXIS(degree, scale, n);
-            GameObject prefapInstance = (GameObject)Instantiate(prefap);
-            prefapInstance.transform.position = new Vector3(PhyllotaxisPosition.x, PhyllotaxisPosition.y, 0);
-            prefapInstance.transform.localScale = new Vector3(scale, scale, scale);
-            n++;
+            StartLerbing();
         }
     }
 
+    private void FixedUpdate()
+    {
+        if (useLerping)
+        {
+            if (islerbing)
+            {
+                float timeSinceStarted = Time.time - timeStartedLerbing;
+                float precentComplete = timeSinceStarted / intervalLerp;
+                transform.localPosition = Vector3.Lerp(startPosition, endPosition, precentComplete);
+                if (precentComplete >= 0.97f) //need optimization
+                {
+                    transform.localPosition = endPosition;
+                    number += stepSize;
+                    currentIteration++;
+                    if (currentIteration <= maxIteration)
+                    {
+                        StartLerbing();
+                    }
+                    else
+                    {
+                        islerbing = false;
+                    }
+                }
+            }
+        }
+        else
+        {
+            PhyllotaxisPosition = CalculatePhyllotXIS(degree, scale, number);
+            transform.localPosition = new Vector3(PhyllotaxisPosition.x, PhyllotaxisPosition.y, 0);
+            number += stepSize;
+            currentIteration++;
+        }
+    }
 
-    private Vector2 CalculatePhyllotXIS(float degree,float scale,int count)
+    void StartLerbing()
+    {
+        islerbing = true;
+        timeStartedLerbing = Time.time;
+        PhyllotaxisPosition = CalculatePhyllotXIS(degree, scale, number);
+        startPosition = this.transform.localPosition;
+        endPosition = new Vector3(PhyllotaxisPosition.x, PhyllotaxisPosition.y, 0);
+
+    }
+
+    private Vector2 CalculatePhyllotXIS(float degree, float scale, int count)
     {
         double angle = count * (degree * Mathf.Deg2Rad);
         float r = scale * Mathf.Sqrt(count);
@@ -36,6 +90,4 @@ public class Phyllotaxis : MonoBehaviour
         Vector2 vec2 = new Vector2(x, y);
         return vec2;
     }
-
-   
 }
